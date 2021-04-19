347fe68180e4721a44bacf80569430cfb4d3e917
Baxtiyar <baxtiyar0393@gmail.com>
2021-04-19 15:50:32 +0400

    first commit

--- src/main/java/az/sibdtm/lesson1/Main.java
+++ src/main/java/az/sibdtm/lesson1/Main.java
@@ -1,26 +1,54 @@
 package az.sibdtm.lesson1;
 
 
+
 import java.util.Scanner;
 
-public class Main {
+public class Main  {
+
 
     public static void main(String[] args) {
 
         Scanner scn=new Scanner(System.in);
 
-        System.out.print("input number1= ");
-        double number1=scn.nextDouble();
-        System.out.print("input operator: ");
-        String operator=scn.next();
-        System.out.print("input number2= ");
-        double number2=scn.nextDouble();
-
-        System.out.println("answer = "+ operators(operator,number1,number2));
-
-
-
+        double number2,number1;
+        System.out.println("Operatorlar:\n " +
+                           "+:toplam, -:çixma, *:vurma, /:bölmə, \n" +
+                           " %:faiz ,pow:qüvvət ,sqrt:kvd kök,abs:modul \n programi bağlamaq:exit ");
+        do {
+            System.out.print("input firstnumber= ");
+             number1 = scn.nextDouble();
+            System.out.print("input operator: ");
+            String operator = scn.next();
+            if(operator.equals("sqrt")||operator.equals("abs")){
+                number2=0.0;
+            }else{
+                System.out.print("input secondnumber= ");
+                number2 = scn.nextDouble();
+            }
+            System.out.println(printResult(operator,number1,number2));
+            System.out.println("programı sonlandırmaq üçün 'exit' konamdsini daxil edin \n əks halda hərhansi bir simvol daxil et");
+            System.out.print("input comand : ");
+            String comand=scn.next();
+            if(comand.equals("exit")) break;
+        }while(true);
     }
+
+
+
+
+     public static String printResult(String operator,double number1,double number2){
+     String result =  operators(operator, number1, number2).toString();
+       if(result.length()-result.indexOf(".")==2&&result.charAt(result.length()-1)=='0'){
+           result= result.substring(0,result.length()-2);
+       }
+       if(number2==0){
+            result=operator+"("+number1+") "+" = "+result;
+       }else result=number1+" "+operator+" "+number2+" = "+result;
+
+    return result;
+    }
+
 
     public static Double operators(String operator,double number1,double number2){
       Double result = null;
@@ -46,15 +74,16 @@ public class Main {
             case "pow":
                 result=Math.pow(number1,number2);
                 break;
-
+            case "sqrt":
+                result=Math.sqrt(number1);
+                break;
+            case "abs":
+                result=Math.abs(number1);
+                break;
 
             default:
                 throw new IllegalStateException("Unexpected value: " + operator);
         }
-
-
-
-    return result;}
-
-
+    return result;
+    }
 }
